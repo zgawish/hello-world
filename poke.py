ec7ad6d19459463eb114d0f0686926636cbb9323
Ziyad Gawish <zgawish@stanford.edu>
2021-07-01 20:34:11 +0000

    added databse functionality

--- poke.py
+++ poke.py
@@ -2,13 +2,14 @@ import sqlalchemy
 from sqlalchemy import create_engine
 import requests
 import pandas as pd
+import os
 
 # use mysql to create schema first
 
 def add_to_table(data, engine):
     print("adding to table")
     dataf = pd.DataFrame(data, index=[0])
-    dataf.to_sql('poke_stats', con=engine, if_exists='replace', index=False)
+    dataf.to_sql('poke_stats', con=engine, if_exists='append', index=False)
 
     
 def get_request(pokemon):
@@ -26,17 +27,18 @@ def handle_request(pokemon):
         exp = response_data['base_experience']
         weight = response_data['weight']
         order = response_data['order']
-        data = {'id': pid, 'name': n, 'height': h, 'base_experience': exp, 'weight': weight, 'order': order}
+        data = {'id': pid, 'name': n, 'height': h, 'base_experience': exp, 'weight': weight, 'rank': order}
         return data
     else:
         return {}
-    
+      
     
 def main():
     # establish connection to db
     print("Creating engine...")
     print("Press enter to cancel")
     engine = create_engine('mysql://root:codio@localhost/poke_data')
+#     os.system("mysql -u  -pcodio poke_data < poke_data.sql")
     pokemon = input("ENTER A POKEMON NAME: ")
     while pokemon != "":
         data = handle_request(pokemon)
@@ -48,8 +50,10 @@ def main():
             add_to_table(data, engine)
         except Exception:
             print("Engine cannot connect. Try checking database first.")
-
+        
+        
         pokemon = input("ENTER A POKEMON NAME: ")
+#     os.system("mysql -u root -pcodio poke_data > ./hello-world/poke_data.sql")
 
     
 if __name__ == '__main__':
