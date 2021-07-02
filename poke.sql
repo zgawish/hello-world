8490c769c911e3a989c207721201048e6ab93370
Ziyad Gawish <zgawish@stanford.edu>
2021-07-02 16:45:26 +0000

    added visualization for poke_data

--- poke.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE poke_stats (
-    id INT(8) UNSIGNED NOT NULL,
-    name VARCHAR(255) default NULL,
-    height INT(8) default NULL,
-    base_experience INT(8) default NULL,
-    weight INT(8) default NULL,
-    rank INT(8) default NULL,
-    PRIMARY KEY(id)
-);
